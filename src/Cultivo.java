e52b0a3722a5521895e30de8cd2459bd38c90b4b
Nico5Ntexto2025 <nicolas.gonzalez2401@alumnos.ubiobio.cl>
2025-09-24 12:20:42 -0300

    Propuesta de Cultivo

--- src/Cultivo.java
+++ src/Cultivo.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cultivo {
+    private int id;
+    private String especie;
+    private String variedad;
+    private float rendimiento;
+
+    private List<Cuartel> cuarteles = new ArrayList<>();
+
+    public Cultivo(int id, String especie, String variedad, float rendimiento) {
+        this.id = id;
+        this.especie = especie;
+        this.variedad = variedad;
+        this.rendimiento = rendimiento;
+    }
+
+    public int getId() {return id;}
+    public String getEspecie() {return especie;}
+    public String getVariedad() {return variedad;}
+    public float getRendimiento() {return rendimiento;}
+    public void setRendimiento(float rendimiento) {
+        this.rendimiento = rendimiento;
+    }
+    public void addCuarteles(Cuartel cuartel) {cuarteles.add(cuartel);}
+    public Cuartel[] getCuarteles() {return cuarteles.toArray(new Cuartel[0]);}
 }

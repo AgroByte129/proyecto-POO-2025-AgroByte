8b73c0ce6beaccda0993a2b92cff75560d6a527a
Nico5Ntexto2025 <nicolas.gonzalez2401@alumnos.ubiobio.cl>
2025-09-27 18:27:57 -0300

    por ahora esta propietario, si su método add requiere de más validación se editará

--- src/Propietario.java
+++ src/Propietario.java
@@ -1,14 +1,17 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Propietario extends Persona{ //extends persona es la herencia
-  private String direccionConmercial;
+public class Propietario extends Persona{
+  private String direccionCom;
   private List<Huerto> huertos = new ArrayList<>();
 
-  public Propietario(String nom,String direccionConmercial) {}
+  public Propietario(Rut rut, String nom, String email, String dir, String direccionCom) {
+    super(rut, nom, email,  dir);
+    this.direccionCom = direccionCom;
+  }
 
-  public String getDireccionComercial(){return direccionComercial;}
-  public void getdireccionComercia(String direccion){direccionComercial = direccion;}
+  public String getDireccionComercial(){return direccionCom;}
+  public void getdireccionComercia(String direccion){direccionCom = direccion;}
   public boolean addHuerto(Huerto huerto){return huertos.add(huerto);} //El ArrayList en Huerto se deberá llamar "huertos"
   public Huerto[] getHuertos(){
     Huerto[] huerto = new Huerto[huertos.size()];

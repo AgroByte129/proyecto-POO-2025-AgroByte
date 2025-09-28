9407ee8df268efe1b70897a3d36c695e594a3287
AgroByte129 <agrobyte129@gmail.com>
2025-09-28 10:25:57 -0300

    creé el Enum "EstadoPlan" y completé lo básico en la CLase "PlanCosecha", incluyendo getters y setters, aunque algunos aún no tienen su interior completo.

--- src/PlanCosecha.java
+++ src/PlanCosecha.java
@@ -1,2 +1,72 @@
+import java.util.Date;
+
 public class PlanCosecha {
+    private int id;
+    private String nombre;
+    private Date inicio;
+    private Date finEstimado;
+    private Date finReal;
+    private double metaKilos;
+    private double precioBaseKilo;
+    private EstadoPlan estado;
+
+    PlanCosecha(int id, String nom, Date ini, Date finEst, double meta, double precio, Cuartel cuartel){
+        this.id = id;
+        this.nombre = nom;
+        this.inicio = ini;
+        this.finEstimado = finEst;
+        this.finReal = finEst;
+        this.metaKilos = metaKilos;
+        this.precioBaseKilo = precioBaseKilo;
+        this.estado = EstadoPlan.PLANIFICADO;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public Date getInicio() {
+        return inicio;
+    }
+    public Date getFinEstimado() {
+        return finEstimado;
+    }
+    public Date getFinReal() {
+        return finReal;
+    }
+    public double getMetaKilos() {
+        return metaKilos;
+    }
+    public void setMetaKilos(double metaKilos) {
+        this.metaKilos = metaKilos;
+    }
+    public double getPrecioBaseKilo() {
+        return precioBaseKilo;
+    }
+    public void setPrecioBaseKilo(double precioBaseKilo) {
+        this.precioBaseKilo = precioBaseKilo;
+    }
+    public EstadoPlan getEstado() {
+        return estado;
+    }
+    public void setEstado(EstadoPlan estado) {
+        this.estado = estado;
+    }
+    public Cuartel getCuartel() {
+
+    }
+    public boolean addCuadrilla(int idCuad, String nomCuadrilla, Supervisor supervisor){
+
+    }
+    public boolean addCosechadorToCuadrilla(int idCuad, Date fIni, Date fFin, double meta, Cosechador cos){
+
+    }
+    public Cuadrilla getCuadrilla(int idCuadrilla){
+
+    }
 }
--- src/Propietario.java
+++ src/Propietario.java
@@ -5,7 +5,9 @@ public class Propietario extends Persona{ //extends persona es la herencia
   private String direccionConmercial;
   private List<Huerto> huertos = new ArrayList<>();
 
-  public Propietario(String nom,String direccionConmercial) {}
+  public Propietario(String nom,String direccionConmercial) {
+
+  }
 
   public String getDireccionComercial(){return direccionComercial;}
   public void getdireccionComercia(String direccion){direccionComercial = direccion;}

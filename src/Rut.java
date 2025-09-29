3c8d41630d55dd1a1c9d812dc4ba831296ae429e
rafit <rafael.figueroa2501@alumnos.ubiobio.cl>
2025-09-29 11:36:18 -0300

    Hice la clase de Rut

--- src/Rut.java
+++ src/Rut.java
@@ -1,2 +1,36 @@
 public class Rut {
+    private String numero;
+
+    public Rut(String numero) {
+        this.numero = numero;
+
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+
+    }
+
+    public String toString() {
+        return numero;
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Rut otro = (Rut) obj;
+        return numero.equals(otro.numero);
+
+    }
+    @Override
+    public int hashCode() {
+        return numero.hashCode();
+    }
 }
+

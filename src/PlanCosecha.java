49f0b3f5d0fcf9ca2de19ca660006f34174cce20
rafit <rafael.figueroa2501@alumnos.ubiobio.cl>
2025-10-01 12:38:43 -0300

    Arrgele el error de rut ya que se usa metodo publico no permitido, y revise plan cosecha y propieatario que no se use metodos publicos

--- src/PlanCosecha.java
+++ src/PlanCosecha.java
@@ -1,2 +1,65 @@
+import java.util.Date;
+import java.util.ArrayList;
+
 public class PlanCosecha {
+    private int id;
+    private String nombre;
+    private Date inicio;
+    private Date finEstimado;
+    private Date finReal;
+    private double metaKilos;
+    private double precioBaseKilo;
+    private EstadoPlan estado;
+    private Cuartel cuartel;
+    private ArrayList<Cuadrilla> cuadrillas;
+
+    public PlanCosecha(int id, String nom, Date ini, Date finEst, double meta, double precio, Cuartel cuartel){
+        this.id = id;
+        this.nombre = nom;
+        this.inicio = ini;
+        this.finEstimado = finEst;
+        this.metaKilos = meta;
+        this.precioBaseKilo = precio;
+        this.cuartel = cuartel;
+        this.estado = EstadoPlan.PLANIFICADO;
+        this.cuadrillas = new ArrayList<>();
+        if (cuartel != null){ cuartel.addPlanCosecha(this); }
+    }
+
+    public int getId(){ return id; }
+    public String getNombre(){ return nombre; }
+    public void setNombre(String nombre){ this.nombre = nombre; }
+    public Date getInicio(){ return inicio; }
+    public void setInicio(Date inicio){ this.inicio = inicio; }
+    public Date getFinEstimado(){ return finEstimado; }
+    public void setFinEstimado(Date finEstimado){ this.finEstimado = finEstimado; }
+    public Date getFinReal(){ return finReal; }
+    public void setFinReal(Date finReal){ this.finReal = finReal; }
+    public double getMetaKilos(){ return metaKilos; }
+    public void setMetaKilos(double metaKilos){ this.metaKilos = metaKilos; }
+    public double getPrecioBaseKilo(){ return precioBaseKilo; }
+    public void setPrecioBaseKilo(double precioBaseKilo){ this.precioBaseKilo = precioBaseKilo; }
+    public EstadoPlan getEstado(){ return estado; }
+    public void setEstado(EstadoPlan estado){ this.estado = estado; }
+    public Cuartel getCuartel(){ return cuartel; }
+
+    public boolean addCuadrilla(int idCuad, String nomCuad, Supervisor supervisor){
+        for (Cuadrilla c : cuadrillas){
+            if (c.getId() == idCuad){ return false; }
+        }
+        Cuadrilla nueva = new Cuadrilla(idCuad, nomCuad, supervisor, this);
+        cuadrillas.add(nueva);
+        return true;
+    }
+
+    public boolean addCosechadorToCuadrilla(int idCuad, Date fIni, Date fFin, double meta, Cosechador cosechador){
+        for (Cuadrilla c : cuadrillas){
+            if (c.getId() == idCuad){ return c.addCosechador(fIni, fFin, meta, cosechador); }
+        }
+        return false;
+    }
+
+    public Cuadrilla[] getCuadrillas(){
+        return cuadrillas.toArray(new Cuadrilla[0]);
+    }
 }
--- src/Rut.java
+++ src/Rut.java
@@ -3,7 +3,6 @@ public class Rut {
 
     public Rut(String numero) {
         this.numero = numero;
-
     }
 
     public String getNumero() {
@@ -12,25 +11,7 @@ public class Rut {
 
     public void setNumero(String numero) {
         this.numero = numero;
-
-    }
-
-    public String toString() {
-        return numero;
-
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Rut otro = (Rut) obj;
-        return numero.equals(otro.numero);
-
-    }
-    @Override
-    public int hashCode() {
-        return numero.hashCode();
     }
 }
+
 

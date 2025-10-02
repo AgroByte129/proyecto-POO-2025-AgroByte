e0ff17752db4567ddc53e30c90bd05a296516fcc
Nico5Ntexto2025 <nicolas.gonzalez2401@alumnos.ubiobio.cl>
2025-10-01 21:52:28 -0300

    cambiado Date a LocalDate

--- src/Cosechador.java
+++ src/Cosechador.java
@@ -1,27 +1,31 @@
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Cosechador extends Persona{
-  private Date fechaNacimiento;
-  private List<CosechadorAsignado> cosAsignados = new ArrayList<>();
+    private LocalDate fechaNacimiento;
+    private List<CosechadorAsignado> cosAsignados = new ArrayList<>();
 
-  public Cosechador(Rut rut, String nom, String email, String dir, Date fNac){
-    super(rut, nom, email, dir);
-    fechaNacimiento = fNac;
-  }
-  
-  public Date getFechaNacimiento(){return fechaNacimiento;}
-  public void setFechaNacimiento(Date fNac){fechaNacimiento = fNac;}
-  public void addCuadrilla(CosechadorAsignado cosAs){
-      cosAsignados.add(cosAs);
-  }
-  public Cuadrilla[] getCuadrillas(){
-    Cuadrilla[] cuadrillas = new Cuadrilla[cosAsignados.size()];
-    for (int i = 0; i < cosAsignados.size(); i++){
-      cuadrillas[i] = cosAsignados.get(i).getCuadrilla();
+    public Cosechador(Rut rut, String nom, String email, String dir, LocalDate fNac){
+        super(rut, nom, email, dir);
+        fechaNacimiento = fNac;
     }
-    return cuadrillas;
+
+    public LocalDate getFechaNacimiento(){return fechaNacimiento;}
+    public void setFechaNacimiento(LocalDate fNac){fechaNacimiento = fNac;}
+    public void addCuadrilla(CosechadorAsignado cosAs){
+        cosAsignados.add(cosAs);
+    }
+    public Cuadrilla[] getCuadrillas(){
+        Cuadrilla[] cuadrillas = new Cuadrilla[cosAsignados.size()];
+        for (int i = 0; i < cosAsignados.size(); i++){
+            cuadrillas[i] = cosAsignados.get(i).getCuadrilla();
+        }
+        return cuadrillas;
+    }
+}
+
+
   }
 }
 

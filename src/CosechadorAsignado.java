e375199579d0e5b8b5fc6d3662c5a05158519df0
Nico5Ntexto2025 <nicolas.gonzalez2401@alumnos.ubiobio.cl>
2025-10-02 10:44:50 -0300

    cambios de Date a LocalDate y eliminacion de metodos :v

--- src/CosechadorAsignado.java
+++ src/CosechadorAsignado.java
@@ -1,15 +1,14 @@
-import java.util.Date;
 import java.time.LocalDate;
 
 public class CosechadorAsignado {
-    private Date desde;
-    private Date hasta;
+    private LocalDate desde;
+    private LocalDate hasta;
     private double metaKilos;
 
     private Cuadrilla cuadrilla;   
     private Cosechador cosechador; 
 
-    public CosechadorAsignado(Date fIni, Date fFin, double meta,
+    public CosechadorAsignado(LocalDate fIni, LocalDate fFin, double meta,
                               Cuadrilla cuad, Cosechador cos) {
         this.desde = fIni;
         this.hasta = fFin;
@@ -18,46 +17,21 @@ public class CosechadorAsignado {
         this.cosechador = cos;
     }
 
-    public LocalDate getDesde() {
-        return convertir(desde);
-    }
-
-    public void setDesde(LocalDate desde) {
-        this.desde = convertir(desde);
-    }
-
-    public LocalDate getHasta() {
-        return convertir(hasta);
-    }
-
-    public void setHasta(LocalDate hasta) {
-        this.hasta = convertir(hasta);
-    }
-
+    public LocalDate getDesde() {return desde;}
+    public void setDesde(LocalDate desde) {this.desde = desde;}
+    public LocalDate getHasta() {return hasta;}
+    public void setHasta(LocalDate hasta) {this.hasta = hasta;}
     public double getMetaKilos() {
         return metaKilos;
     }
-
     public void setMetaKilos(double metaKilos) {
         this.metaKilos = metaKilos;
     }
-
     public Cuadrilla getCuadrilla() {
         return cuadrilla;
     }
-
     public Cosechador getCosechador() {
         return cosechador;
-    }
-
-    private LocalDate convertir(Date fecha) {
-        if (fecha == null) return null;
-        return new java.sql.Date(fecha.getTime()).toLocalDate();
-    }
-
-    private Date convertir(LocalDate fecha) {
-        if (fecha == null) return null;
-        return java.sql.Date.valueOf(fecha);
     }
 }
 

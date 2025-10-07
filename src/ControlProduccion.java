8d58b68d5f53b69268b29b0095f24bb586bd7065
Nico5Ntexto2025 <nicolas.gonzalez2401@alumnos.ubiobio.cl>
2025-10-07 10:26:02 -0300

    terminado método addCosechadorToCuadrilla y agregado métodos privados buscar para facilitar el código.

--- src/ControlProduccion.java
+++ src/ControlProduccion.java
@@ -10,33 +10,15 @@ public class ControlProduccion {
     public ControlProduccion(){}
 
     public boolean createPropietario(Rut rut, String nombre, String email, String dirParticular, String dirComercial){
-        for(int i = 0; i < personas.size(); i++){
-            if(personas.get(i).getRut().equals(rut)){
-                return false;
-            }
-        }
-        Propietario p = new Propietario(rut, nombre, email, dirParticular, dirComercial);
-        return personas.add(p);
+        return buscaPersona(rut) == null && personas.add(new Propietario(rut, nombre, email, dirParticular, dirComercial));
     }
 
     public boolean createSupervisor(Rut rut, String nombre, String email, String direccion, String profesion){
-        for(int i = 0; i < personas.size(); i++){
-            if(personas.get(i).getRut().equals(rut)){
-                return false;
-            }
-        }
-        Supervisor p = new Supervisor(rut, nombre, email, direccion, profesion);
-        return personas.add(p);
+        return buscaPersona(rut) == null && personas.add(new Supervisor(rut, nombre, email, direccion, profesion));
     }
 
     public boolean createCosechador(Rut rut, String nom, String email, String direccion, LocalDate fNac){
-        for(int i = 0; i < personas.size(); i++){
-            if(personas.get(i).getRut().equals(rut)){ //no c como usar otro metodo para comparar sin equals :'v
-                return false;
-            }
-        }
-        Cosechador c = new Cosechador(rut, nom, email, direccion, fNac);
-        return personas.add(c);
+        return buscaPersona(rut) == null && personas.add(new Cosechador(rut, nom, email, direccion, fNac));
     }
 
     public boolean createCultivo(int id, String especie, String variedad, float rendimiento){

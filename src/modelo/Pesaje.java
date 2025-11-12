21c6d35e396d2c2b33a9ab8b9bc1ba3b5f696d0c
Nico5Ntexto2025 <nicolas.gonzalez2401@alumnos.ubiobio.cl>
2025-11-12 20:30:04 -0300

    correccion en método getMonto de Pesaje, ahora usa el precioKg de la instancia y no lo vuelve a pedir al plan.

--- src/modelo/Pesaje.java
+++ src/modelo/Pesaje.java
@@ -31,17 +31,13 @@ public class Pesaje {
     public LocalDateTime getFechaHora() {return fechaHora;}
     public double getPrecioKg() {return precioKg;}
     public double getMonto(){
-        double precioBase = 0.0;
-        if (cosAsig != null && cosAsig.getCuadrilla() != null && cosAsig.getCuadrilla().getPlanCosecha() != null) {
-            precioBase = cosAsig.getCuadrilla().getPlanCosecha().getPrecioBaseKilo();
-        }
         double factor = switch (calidad) {
-            case EXCELENTE -> 1.0;
+            case EXCELENTE -> 1.0; //redundante pero deja explícito que si es excelente se deja el precio completo
             case SUFICIENTE -> 0.8;
             case DEFICIENTE -> 0.6;
             default -> 1.0;
         };
-        return precioBase * cantidadKg * factor;
+        return precioKg * cantidadKg * factor;
     }
     public CosechadorAsignado getCosechadorAsignado() {return cosAsig;}
     public void setPago(PagoPesaje pago){

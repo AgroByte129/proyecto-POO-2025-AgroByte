6f0f9167a72fb0ecbe402f468e86d290afd3523c
Nico5Ntexto2025 <nicolas.gonzalez2401@alumnos.ubiobio.cl>
2025-11-28 18:24:49 -0300

    Creado métodos save en clase GestionHuertosIO (savePersonas, saveCultivos, savePlanesCosecha).

--- src/persistencia/GestionHuertosIO.java
+++ src/persistencia/GestionHuertosIO.java
@@ -1,12 +1,59 @@
 package persistencia;
 
+import modelo.Cultivo;
+import modelo.Persona;
+import modelo.PlanCosecha;
+import utilidades.GestionHuertosException;
+
+import java.io.*;
+import java.util.Arrays;
+
 public class GestionHuertosIO {
     private static GestionHuertosIO instance;
-    private GestionHuertosIO(){}
+
+    private GestionHuertosIO() {
+    }
+
     public static GestionHuertosIO getInstance() {
         if (instance == null) {
             instance = new GestionHuertosIO();
         }
         return instance;
+    }
+
+    public void savePersonas(Persona[] personas) throws GestionHuertosException {
+        try (ObjectOutputStream archivo =
+                     new ObjectOutputStream(new FileOutputStream("Personas.obj"))) {
+            for (Persona p : personas)
+                archivo.writeObject(p);
+        } catch (NotSerializableException nSE) {
+            throw new GestionHuertosException("Alguna clase relacionada no implementa la interfaz serializable.");
+        } catch (IOException iOE) {
+            throw new GestionHuertosException("IOException: Hubo un error al intentar guardar los datos.");
+        }
+    }
+
+    public void saveCultivos(Cultivo[] cultivos) throws GestionHuertosException {
+        try (ObjectOutputStream archivo =
+                     new ObjectOutputStream(new FileOutputStream("Cultivos.obj"))) {
+            for (Cultivo c : cultivos)
+                archivo.writeObject(c);
+        } catch (NotSerializableException nSE) {
+            throw new GestionHuertosException("Alguna clase relacionada no implementa la interfaz serializable.");
+        } catch (IOException iOE) {
+            throw new GestionHuertosException("IOException: Hubo un error al intentar guardar los datos.");
+        }
+    }
+
+    public void savePlanesCosecha(PlanCosecha[] planes) throws GestionHuertosException{
+        try(ObjectOutputStream archivo =
+                    new ObjectOutputStream(new FileOutputStream("PlanesCosecha.obj"))){
+            for(PlanCosecha pC: planes)
+                archivo.writeObject(pC);
+        }catch(NotSerializableException nSE){
+            throw new GestionHuertosException("Alguna clase relacionada no implementa la interfaz serializable.");
+        } catch (IOException iOE) {
+            throw new GestionHuertosException("IOException: Hubo un error al intentar guardar los datos.");
+        }
     }
 }

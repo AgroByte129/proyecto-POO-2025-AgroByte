2a9885d53dccbc3646a2d956062d3cf3d98a9d2f
AgroByte129 <agrobyte129@gmail.com>
2025-12-11 21:36:56 -0300

    new CambioDeEstadoPlanDialog().setVisible(true); ha sido agregado en el menu del UI, se han hecho correcciones en ControladorProduccion, con los espacios dentro de "%s;%s;%s" (getCuadrillasDeCosechadorDePlan), se han agregado las opciones 5 y 6 del menu en el UI

--- src/vista/CambioDeEstadoPlanDialog.java
+++ src/vista/CambioDeEstadoPlanDialog.java
@@ -11,7 +11,7 @@ public class CambioDeEstadoPlanDialog extends JDialog {
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
-//comentario para commit correcciones
+//comentario para commit correcciones nuevas
     private JLabel cambioDeEstadoPlanLabel;
     private JLabel nombreLabel;
     private JLabel cumplimientoDeMetaLabel;

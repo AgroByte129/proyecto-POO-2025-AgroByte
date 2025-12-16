cd69b6068359aeccd9cfe56ad0b7f3d2622d1618
AgroByte129 <agrobyte129@gmail.com>
2025-12-16 19:20:27 -0300

    propuesta de correcciones y mejoras en CrearPersonaGUI

--- src/vista/CrearPersonaGUI.java
+++ src/vista/CrearPersonaGUI.java
@@ -2,7 +2,9 @@ package vista;
 //Rafael Ignacio Figueroa Espinoza no terminado
 
 
-// te sale arturo?
+// te sale arturo? sí
+import controlador.ControladorProduccion;
+
 import javax.swing.*;
 import java.awt.event.*;
 
@@ -22,11 +24,35 @@ public class CrearPersonaGUI extends JDialog {
     private JRadioButton rbPropietario;
     private JRadioButton rbSupervisor;
     private JRadioButton rbCosechador;
+    private JLabel rutLabel;
+    private JLabel nombreLabel;
+    private JLabel emailLabel;
+    private JLabel direccionLabel;
+    private JLabel rolLabel;
+
+    private JLabel extraLabel; //le puse de nombre extra, porque funcionara para dir comercial y para profesion
+    private JLabel fechaNacimientoLabel;
+    private JLabel creacionDePersonasLabel;
+
+    private ControladorProduccion cp = ControladorProduccion.getInstance();
 
     public CrearPersonaGUI() {
         setContentPane(contentPane);
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
+
+        ButtonGroup grupoRoles = new ButtonGroup(); //defino el grupo
+        grupoRoles.add(rbPropietario); //agrego cada uno de los rb al grupo
+        grupoRoles.add(rbSupervisor);
+        grupoRoles.add(rbCosechador);
+
+        ActionListener listenerCambioRol = e -> actualizarCampos(); //creo un actionlistener para cada uno, con un metod que me deja cambiar la visibilidad de los campos de texto
+        rbPropietario.addActionListener(listenerCambioRol);
+        rbSupervisor.addActionListener(listenerCambioRol);
+        rbCosechador.addActionListener(listenerCambioRol);
+
+        rbPropietario.setSelected(true); //lo dejo seleccionado por defecto, para que se vea mejor XD
+        actualizarCampos();
 
         buttonOK.addActionListener(e -> onOK());
         buttonCancel.addActionListener(e -> onCancel());
@@ -37,6 +63,37 @@ public class CrearPersonaGUI extends JDialog {
                 onCancel();
             }
         });
+
+        pack(); //pongo estas cosas aca para que funcionen cuando se ponga en el menu y no solo con el main de la misma clase
+        setLocationRelativeTo(null);
+        setAlwaysOnTop(true);
+    }
+
+    private void actualizarCampos() { //este es el metod
+
+        extraLabel.setVisible(false); //hago que no se vean los labels y sus respectivos text fields
+        txtExtra.setVisible(false);
+        fechaNacimientoLabel.setVisible(false);
+        txtFechaNac.setVisible(false);
+
+        if (rbPropietario.isSelected()) { //se hace el cambio, viendo cuál fue seleccionado y escondiendo los que no lo están
+
+            extraLabel.setText("Dirección Comercial:");
+            extraLabel.setVisible(true);
+            txtExtra.setVisible(true);
+
+        } else if (rbSupervisor.isSelected()) {
+
+            extraLabel.setText("Profesión:");
+            extraLabel.setVisible(true);
+            txtExtra.setVisible(true);
+
+        } else if (rbCosechador.isSelected()) {
+
+            fechaNacimientoLabel.setVisible(true);
+            txtFechaNac.setVisible(true);
+        }
+        pack();//para ajustar el tamaño
     }
 
     private void onOK() {
@@ -49,9 +106,14 @@ public class CrearPersonaGUI extends JDialog {
     }
 
     public static void main(String[] args) {
+
+        try {
+            ControladorProduccion.getInstance().readDataFromTextFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         CrearPersonaGUI dialog = new CrearPersonaGUI();
-        dialog.pack();
-        dialog.setLocationRelativeTo(null);
         dialog.setVisible(true);
         System.exit(0);
     }
